51c57c95ab730732f0efd765b48b29a745cd61af
Tiffany Chen <tiffanysta.c@gmail.com>
2023-01-11 19:47:28 -0500

    attempts were made

--- api/sentiment-analysis/model.py
+++ api/sentiment-analysis/model.py
@@ -25,20 +25,20 @@ def clean_text(text):
 
     # compute things
     collection = TextCollection(tokens)
-    token_set = set(pos_tags)
     new_text = "".join([t + " " for t in tokens])
-    pos = 0
-    neg = 0
-    for t in token_set:
+
+    pos = neg = neu = 0
+    for t in pos_tags:
         synsets = wn.synsets(t[0], pos=t[1]) # grab the most common one
         if not synsets:
             continue
         synset = synsets[0]
         swn_synset = swn.senti_synset(synset.name())
-        pos += swn_synset.pos_score() * collection.tf_idf(t[0], new_text) 
-        neg += swn_synset.neg_score() * collection.tf_idf(t[0], new_text) 
-
-    return (pos, neg)
+        pos += swn_synset.pos_score() 
+        neg += swn_synset.neg_score()
+        neu += swn_synset.obj_score() 
+        #  collection.tf_idf(t[0], new_text) 
+    return (pos / len(new_text.split()), neg / len(new_text.split()), neu / len(new_text.split()))
 
 
 def penn_to_wn(tag):
@@ -57,17 +57,20 @@ def penn_to_wn(tag):
 def test_model_func():
     pos_count = 0
     neg_count = 0
+    neu_count = 0
     for t in ["business", "entertainment", "politics", "sport", "tech"]:
         path = "../../data/bbc/" + t
         for filename in os.listdir(path):
             with open(path + "/" + filename) as f:
                 lines = f.readlines()
                 text = "".join(lines)
-                pos, neg = clean_text(text)
+                pos, neg, _ = clean_text(text)
                 if pos > neg:
                     pos_count += 1
-                else:
+                elif pos < neg:
                     neg_count += 1
-    return pos_count, neg_count
+                else:
+                    neu_count += 1
+    return pos_count, neg_count, neu_count
 
 print(test_model_func())
