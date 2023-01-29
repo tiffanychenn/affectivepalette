92dd03bc78ffd95fad7ff487ebce3bf06901caac
Tiffany Chen <tiffanysta.c@gmail.com>
2023-01-28 23:29:22 -0500

    gonna quickly push even though things are broken :( but started out the panas form for the browser extension

---
+++ browser-extension/popup.js
@@ -0,0 +1,65 @@
+const _submitPanasForm = () => {
+  const interested = document.querySelector('input[name="interested"]:checked').value;
+  const distressed = document.querySelector('input[name="distressed"]:checked').value;
+  const excited = document.querySelector('input[name="excited"]:checked').value;
+  const upset = document.querySelector('input[name="upset"]:checked').value;
+  const strong = document.querySelector('input[name="strong"]:checked').value;
+  const guilty = document.querySelector('input[name="guilty"]:checked').value;
+  const scared = document.querySelector('input[name="scared"]:checked').value;
+  const hostile = document.querySelector('input[name="hostile"]:checked').value;
+  const enthusiastic = document.querySelector('input[name="enthusiastic"]:checked').value;
+  const proud = document.querySelector('input[name="proud"]:checked').value;
+  const irritable = document.querySelector('input[name="irritable"]:checked').value;
+  const alert = document.querySelector('input[name="alert"]:checked').value;
+  const ashamed = document.querySelector('input[name="ashamed"]:checked').value;
+  const inspired = document.querySelector('input[name="inspired"]:checked').value;
+  const nervous = document.querySelector('input[name="nervous"]:checked').value;
+  const determined = document.querySelector('input[name="determined"]:checked').value;
+  const attentive = document.querySelector('input[name="attentive"]:checked').value;
+  const jittery = document.querySelector('input[name="jittery"]:checked').value;
+  const active = document.querySelector('input[name="active"]:checked').value;
+  const afraid = document.querySelector('input[name="afraid"]:checked').value;
+
+  const panasFormResults = {
+    "interested": interested,
+    "distressed": distressed,
+    "excited": excited,
+    "upset": upset,
+    "strong": strong,
+    "guilty": guilty,
+    "scared": scared,
+    "hostile": hostile,
+    "enthusiastic": enthusiastic,
+    "proud": proud,
+    "irritable": irritable,
+    "alert": alert,
+    "ashamed": ashamed,
+    "inspired": inspired,
+    "nervous": nervous,
+    "determined": determined,
+    "attentive": attentive,
+    "jittery": jittery,
+    "active": active,
+    "afraid": afraid
+  }
+
+  fetch('http://127.0.0.1:5000/panas', {
+    method: "POST",
+    body: panasFormResults,
+    headers: {
+      'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+    },
+  }).then(response => {
+    return response.json();
+  }).then(data =>{
+      console.log(data);
+  }).catch(error => {
+    console.log(error);
+  })
+  
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("submit").onclick = _submitPanasForm;
+});
